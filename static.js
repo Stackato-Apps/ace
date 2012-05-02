e230d8465da05da224afba2cd755809d40516acc
Jamie Paton <jamie.paton@googlemail.com>
2012-05-02 00:23:32 -0700

    Make ace friends with Stackato.

--- static.js
+++ static.js
@@ -5,7 +5,7 @@ var http = require("http"),
     path = require("path"),
     fs = require("fs"),
     mime = require("mime"),
-    port = process.env.PORT || 8888;
+    port = process.env.VCAP_APP_PORT || process.env.PORT || 8888;
 
 http.createServer(function(request, response) {
 
